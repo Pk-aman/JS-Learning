38d1d2955f2b7a6afead51ba324d36f35329c6b2
Aman Kumar <63278032+Pk-aman@users.noreply.github.com>
2025-03-31 19:47:00 +0000

    add pproblems

--- Basics/test.js
+++ Basics/test.js
@@ -1,1 +1,22 @@
 console.log("Hello World!");
+
+// Problem 1
+// const str = "Pankaj"  // P n k j a a
+
+// // Aman  -> n m a A
+
+// const strArr = str.split('');
+// strArr.sort().reverse();
+
+// console.log(strArr)
+
+// Problem 2
+const str = "I love my India";
+const ans = str.split(" ").map((word) => {return (word.split('').reverse().join(''));}).join(" ");
+console.log(ans);
+// const strArr = str.split(" ")
+// console.log(strArr);
+// const strRev = strArr.map((word) => {return (word.split('').reverse().join())})
+// console.log(strRev);
+// const ans = strRev.join(" ");
+// console.log(ans);
